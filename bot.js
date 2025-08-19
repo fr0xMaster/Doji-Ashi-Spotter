7d12750a2f981de255595617a1bd4bd966457f7c
fr0xMaster <fr0xmaster@protonmail.com>
2025-08-19 16:28:47 +0200

    fix some bugs, and add list of symbols

--- bot.js
+++ bot.js
@@ -1,9 +1,14 @@
 import cron from "node-cron";
+import fs from "fs";
+import path from "path";
 import { fetchCandles } from "./lib/ccxtHelper.js";
 import { calculateHA } from "./lib/ha.js";
 import { calculateRSI } from "./lib/rsi.js";
 import { sendMessage } from "./lib/telegram.js";
-import { symbols } from ".lib/symbols.js";
+import { symbols } from "./lib/symbols.js";
+import { symbolForHL, symbolForTG } from "./lib/format.js";
+
+const logFilePath = path.resolve("./error.log"); // Fichier de log
 
 async function analyzeSymbol(symbol) {
   const tf = "1h";
@@ -23,22 +28,37 @@ async function analyzeSymbol(symbol) {
     0.1;
 
   if (isDoji && lastRSI < 30) {
-    const message = `🚨 ${symbol} - ${tf} : Doji détecté sur la dernière bougie avec RSI = ${lastRSI.toFixed(
+    const message = `🚨 ${symbolForTG(
+      symbol
+    )} - ${tf} : Doji détecté sur la dernière bougie avec RSI = ${lastRSI.toFixed(
       2
     )}`;
     sendMessage(message);
   } else {
     sendMessage(
-      `Pas de Doji pour ${symbol} sur UT ${tf} . RSI: ${lastRSI.toFixed(2)}`
+      `Pas de Doji pour ${symbolForTG(
+        symbol
+      )} sur UT ${tf} . RSI: ${lastRSI.toFixed(2)}`
     );
   }
 }
 
-// Planifier l'analyse toutes les heures à la minute 55
+// Analyse toutes les 5 minutes
 cron.schedule("*/5 * * * *", async () => {
   console.log("🔎 Lancement de l’analyse H1 pour toutes les cryptos...");
+
   for (const symbol of symbols) {
-    const symbolFormattedForHL = symbol + "/USDC:USDC";
-    await analyzeSymbol(symbolFormattedForHL);
+    try {
+      await analyzeSymbol(symbolForHL(symbol));
+    } catch (error) {
+      console.error(`⚠️ Erreur sur ${symbolForTG(symbol)} :`, error.message);
+      // Écriture dans le fichier log
+      fs.appendFileSync(
+        logFilePath,
+        `[${new Date().toISOString()}] ${errorMessage}\n`
+      );
+      sendMessage(`⚠️ Erreur sur ${symbolForTG(symbol)} : ${error.message}`);
+      continue;
+    }
   }
 });
